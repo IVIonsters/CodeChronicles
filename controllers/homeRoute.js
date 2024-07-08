bb7db3e31fd9fca10ecd9cab6e54aa7763aa8b3c
Zachary Polof <ZacharyPolof@gmail.com>
2024-07-07 21:16:29 -0500

    Update homeRoute.js
    Defined routes for single posts
    
    setup route to control dashboard

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -30,3 +30,62 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// GET one post
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [{ model: User, attributes: ['username'] }],
+        });
+        if (!postData) {
+            res.status(404).json({ message: 'No Post found, incorrect ID' });
+            return;
+        }
+        const post = postData.get({ plain: true });
+        post.formattedDate = formatDate(post.created_on);
+
+        res.render('posts', {
+            ...post,
+            loggedIn: req.session.loggedIn,
+            pageTitle: 'Single Post',
+        });
+    } catch (error) {
+        console.error('post route error', error);
+        res.status(500).json(error);
+    }
+});
+
+//Dashboard route
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.userId, {
+            attributes: { exclude: ['password'] },
+            include: [{
+                model: Post,
+                where: {
+                    userId: req.session.user_id,
+                },
+                required: false,
+            }],
+        });
+        if (!userData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+
+        const user = userData.get({ plain: true });
+        user.posts = user.posts.map((post) => {
+            post.formattedDate = formatDate(post.created_on);
+            return post;
+        });
+
+        res.render('dashboard', {
+            ...user,
+            loggedIn: true,
+            pageTitle: 'Dashboard',
+        });
+    } catch (err) {
+        console.error('dashboard route error', err);
+        res.status(500).json(err);
+    }
+});
