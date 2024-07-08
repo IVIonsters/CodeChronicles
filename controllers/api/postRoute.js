d4705269e330f7b4520e774fb9452277b84230fb
Zachary Polof <ZacharyPolof@gmail.com>
2024-07-07 21:58:41 -0500

    Update postRoute.js
    added routes to update post , delete post.
    
    added verification to delete route for user

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -34,3 +34,49 @@ router.post('/', withAuth, async (req, res) => {
         res.status(500).json(error);
     }
 });
+
+//Update post route
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const [updatedPost] = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+                userId: req.session.userId,
+            },
+        });
+        if (updatedPost > 0) {
+            res.status(200).end();
+        } else {
+            res.status(404).json({ message: 'No post found with this id!' });
+        }
+    } catch (error) {
+        console.error('Error in updating post: ', error);
+        res.status(500).json(error);
+    }
+});
+
+//Delete post route
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const deletedPost = await Post.findByPk(req.params.id);
+        if (!deletedPost) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        if (deletedPost.userId !== req.session.userId) {
+            res.status(403).json({ message: 'You cannot delete this post!' });
+            return;
+        }
+        await Post.destroy({
+            where: {
+                id: req.params.id,
+                userId: req.session.userId,
+            },
+        });
+    } catch (error) {
+        console.error('Error in deleting post: ', error);
+        res.status(500).json(error);
+    }
+});
+
+module.exports = router;
